1d86ed19ec9b259a7da153d4e1a16781ec3cef82
pratikbhattarai76 <pratikbhattarai76@gmail.com>
2026-04-02 08:30:06 +0545

    added line breaks and improved formatting

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -3,23 +3,23 @@
 echo "Analysing Log Files"
 echo "==================="
 
-echo "List of log files updated in last 24 hours"
+echo -e "\nList of log files updated in last 24 hours"
 find . -name "*.log" -mtime -1
 
-echo "searching ERROR logs in application.log file"
+echo -e "\nsearching ERROR logs in application.log file"
 grep "ERROR" logs/application.log
 
-echo "Number of ERROR logs found in application.log"
+echo -e "\nNumber of ERROR logs found in application.log"
 grep -c "ERROR" logs/application.log
 
-echo "Number of FATAL logs found in application.log"
+echo -e "\nNumber of FATAL logs found in application.log"
 grep -c "FATAL" logs/application.log
 
-echo "Number of FATAL logs found in system.log"
+echo -e "\nNumber of FATAL logs found in system.log"
 grep -c "FATAL" logs/system.log
 
-echo "Number of CRITICAL logd found in system.log"
+echo -e "\nNumber of CRITICAL logd found in system.log"
 grep -c "CRITICAL" logs/system.log
 
-echo "Critical logs in system.log file"
+echo -e "\nCritical logs in system.log file"
 grep "CRITICAL" logs/system.log
