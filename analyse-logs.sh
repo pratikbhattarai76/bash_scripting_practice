2e975280781524da2c38a204b7f3902f3ddb0684
pratikbhattarai76 <pratikbhattarai76@gmail.com>
2026-04-02 08:26:36 +0545

    added echo for understanding

--- analyse-logs.sh
+++ analyse-logs.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+
+echo "Analysing Log Files"
+echo "==================="
+
+echo "List of log files updated in last 24 hours"
 find . -name "*.log" -mtime -1
 
+echo "searching ERROR logs in application.log file"
 grep "ERROR" logs/application.log
+
+echo "Number of ERROR logs found in application.log"
 grep -c "ERROR" logs/application.log
+
+echo "Number of FATAL logs found in application.log"
 grep -c "FATAL" logs/application.log
 
+echo "Number of FATAL logs found in system.log"
 grep -c "FATAL" logs/system.log
+
+echo "Number of CRITICAL logd found in system.log"
 grep -c "CRITICAL" logs/system.log
+
+echo "Critical logs in system.log file"
 grep "CRITICAL" logs/system.log
